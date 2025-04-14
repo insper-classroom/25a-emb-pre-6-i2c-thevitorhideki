63235b14aeddb5850a76d9ea25fc0ecabd95d76a
Vitor Katakura <7vitorgomez@gmail.com>
2025-04-14 10:37:01 -0300

    exercício 3

--- exe3/main.c
+++ exe3/main.c
@@ -10,7 +10,7 @@
 #include "hardware/i2c.h"
 #include "mpu6050.h"
 
-const int I2C_CHIP_ADDRESS = 0x68;
+const int I2C_CHIP_ADDRESS = 0x76;
 const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
 
@@ -21,9 +21,12 @@ void i2c_task(void *p) {
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    // TODO
-    // read id chip BMP280
-    // printf("BMP280 ID: 0x%X \n", buffer[0]);
+    uint8_t buffer[7];
+
+    uint8_t reg_address = 0xD0;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
+    printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
