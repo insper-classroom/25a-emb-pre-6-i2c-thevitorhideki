58d4f73d1fcce49db8b20bad8dab7fead9558990
Vitor Katakura <7vitorgomez@gmail.com>
2025-04-14 09:48:27 -0300

    exercício 1

--- exe1/main.c
+++ exe1/main.c
@@ -28,8 +28,11 @@ void i2c_task(void *p) {
     buf_write[1] = 1 << 7;            // valor
     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
 
-    // TODO
-    // Configure o acc para operar em 4G
+    unsigned int range = 1;
+    uint8_t buf_write_acc[2];
+    buf_write_acc[0] = MPUREG_ACCEL_CONFIG;
+    buf_write_acc[1] = range;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write_acc, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
